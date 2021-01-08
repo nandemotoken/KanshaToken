d742cc5446ffd96fdc6c7c58adbaf6c63bb05095
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2021-01-08 23:19:35 +0900

    Update read_write.js

--- read_write.js
+++ read_write.js
@@ -11,9 +11,9 @@ await torus.init({
   buildEnv: "production", // default: production
   enableLogging: true, // default: false
   network: {
-    host: "matic", // default: mainnet
-    chainId: 137, // default: 1
-    networkName: "Matic Mainnet" // default: Main Ethereum Network
+    host: "xdai", // default: mainnet
+    chainId: 100, // default: 1
+    networkName: "xdai stable" // default: Main Ethereum Network
   },
   showTorusButton: true // default: true
 });
